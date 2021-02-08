fe829a5e12382c5852658c6aa94e3efa85a5646c
Barbara Ramos <barbara.ramos@zentagroup.com>
2021-02-07 21:04:22 -0300

    checkbox functionality test

--- src/App.js
+++ src/App.js
@@ -3,17 +3,27 @@ import "./App.css";
 
 const App = () => {
   const [buttonIsRed, setButtonIsRed] = useState(true);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
 
   return (
     <div>
       <button
         style={{ backgroundColor: buttonIsRed ? "red" : "blue" }}
+        disabled={buttonDisabled}
         onClick={() => {
           setButtonIsRed(!buttonIsRed);
         }}
       >
         {buttonIsRed ? "Change to blue" : "Change to red"}
       </button>
+      <input 
+        type="checkbox" 
+        defaultChecked={buttonDisabled}
+        aria-checked={buttonDisabled}
+        onChange={() => {
+          setButtonDisabled(!buttonDisabled)
+        }}
+      />
     </div>
   );
 };
--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,6 @@ test('button is red and it turns blue when clicked', () => {
   render(<App />);
 
   const colorButton = screen.getByRole('button', {name: 'Change to blue'});
-
   expect(colorButton).toHaveStyle({backgroundColor: 'red'});
 
   fireEvent.click(colorButton);
@@ -14,3 +13,32 @@ test('button is red and it turns blue when clicked', () => {
   expect(colorButton.textContent).toBe('Change to red');
   
 });
+
+test('initial condition', () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+  expect(colorButton).toBeEnabled();
+
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked()
+
+})
+
+test('checkbox functionality', () => {
+  render(<App />);
+
+  const checkbox = screen.getByRole('checkbox');
+  const colorButton = screen.getByRole('button', {name: 'Change to blue'});
+
+  fireEvent.click(checkbox);
+
+  expect(checkbox).toBeChecked();
+  expect(colorButton).toBeDisabled();
+
+  fireEvent.click(checkbox);
+
+  expect(checkbox).not.toBeChecked();
+  expect(colorButton).toBeEnabled();
+
+})
